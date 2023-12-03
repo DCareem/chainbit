18b1fb65cf7194e7f7e394d90ca53075a8de9fa2
Drissi Karim <126094903+AzulianW@users.noreply.github.com>
2023-12-03 03:29:19 +0100

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(
+    name="myproject",
+    versioning="distance",          # Optional, would activate tag-based versioning
+    setup_requires="setupmeta"      # This is where setupmeta comes in
+)
